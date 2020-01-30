0dc7aaeea62f82f731700f2c03bd3e5023856f37
prasanna <prasanna@192.168.0.184>
2020-01-30 08:37:25 +0530

    f3 method added

--- myproj/src/Yahoo_Prg/Sample.java
+++ myproj/src/Yahoo_Prg/Sample.java
@@ -5,5 +5,13 @@ public class Sample
 public void f1()
 {
 	System.out.println("hello");
+	System.out.println("added one more");
 }
+public void f3()
+{
+	System.out.println("f3 added");
+	
+}
+
+
 }
